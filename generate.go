fd23b9bb8cade94731f541ca12e642bfdf5cd369
Oleg Jukovec <oleg.jukovec@tarantool.org>
2023-04-20 22:49:45 +0300

    api: add Iterator
    Relates to https://github.com/tarantool/go-tarantool/pull/251

--- generate.go
+++ generate.go
@@ -4,6 +4,7 @@ package iproto
 //go:generate stringer -type=Error
 //go:generate stringer -type=Feature
 //go:generate stringer -type=Flag
+//go:generate stringer -type=Iterator
 //go:generate stringer -type=Type
 //go:generate stringer -type=Key,MetadataKey,BallotKey,RaftKey,SqlInfoKey -output=keys_string.go
 //go:generate goimports -w .
